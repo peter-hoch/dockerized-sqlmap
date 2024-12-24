037bcc51815ae89a2145f3b6dd18e9ae616ee8d6
Peter Hoch <peter-hoch@users.noreply.github.com>
2024-12-24 07:59:17 -0500

    script update

--- push.sh
+++ push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-docker push petersdockerid0815/sqlmap:1.8.12 .
-docker push petersdockerid0815/sqlmap:latest .
-
+docker push petersdockerid0815/sqlmap:1.8.12 
+docker push petersdockerid0815/sqlmap:latest 
 
